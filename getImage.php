381869f9903338342079b1de592b51dd0fa1daac
Warren Harrison <warren@hungry-media.com>
2013-08-25 10:15:39 -0400

    Cleaned up formatting

--- getImage.php
+++ getImage.php
@@ -2,22 +2,22 @@
 $okExts = array( 'png', 'jpg', 'gif', 'jpeg' );
 $imgURL = $_SERVER['QUERY_STRING'];
 if( $_SERVER['QUERY_STRING'] ){
-	$imgBits = explode( '.', $imgURL );
-	$ext = strtolower( array_pop( $imgBits ) );
-	if( !in_array( $ext, $okExts ) ){ echo "FAIL"; exit(); }
-	switch( $ext ){
-		case 'png':
-			header('Content-type: image/png');
-			break;
-		case 'jpeg':
-		case 'jpg':
-			header('Content-type: image/jpeg');
-			break;
-		case 'gif':
-			header('Content-type: image/gif');
-			break;
-	}
-	readfile( $imgURL );
+  $imgBits = explode( '.', $imgURL );
+  $ext = strtolower( array_pop( $imgBits ) );
+  if( !in_array( $ext, $okExts ) ){ echo "FAIL"; exit(); }
+  switch( $ext ){
+  case 'png':
+    header('Content-type: image/png');
+    break;
+  case 'jpeg':
+  case 'jpg':
+    header('Content-type: image/jpeg');
+    break;
+  case 'gif':
+    header('Content-type: image/gif');
+    break;
+  }
+  readfile( $imgURL );
 }
 
 ?>
